d54657959b2eaa8df56137f9a60eb1ba8cd5702f
camryn williams <williams.camryn.e@gmail.com>
2021-06-28 09:51:34 -0400

    commit tree fc88f7ff81e274bf61794e23041d0f714857cb21

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,27 @@
+class PlantsController < ApplicationController
+
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plant = Plant.find(params[:id])
+        render json: plant
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    def update
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+end
